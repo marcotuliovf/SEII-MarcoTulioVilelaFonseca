2437d4c10d597c65852ec86a5e3f2f5fbc14bbc7
marcotuliovf <marcotuliovf>
2024-09-08 18:54:17 -0300

    Arquivos atualizados com sucesso!

---
+++ Semana04/Exercicio02/Video02/01Conceitos/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01Conceitos
+{
+    class Porgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("!"); 
+        }
+    }
+}
