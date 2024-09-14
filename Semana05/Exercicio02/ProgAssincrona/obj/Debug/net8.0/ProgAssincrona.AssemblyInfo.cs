85168eb27c53a1353054e80768e362dfcfa4232f
marcotuliovf <marcotuliovf>
2024-09-14 12:40:56 -0300

    Arquivos atualizados com sucesso!

--- Semana05/Exercicio02/ProgAssincrona/obj/Debug/net8.0/ProgAssincrona.AssemblyInfo.cs
+++ Semana05/Exercicio02/ProgAssincrona/obj/Debug/net8.0/ProgAssincrona.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("14Interface")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ProgAssincrona")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cdd01d67fe26951a08aba40e18859042b56d26a")]
-[assembly: System.Reflection.AssemblyProductAttribute("14Interface")]
-[assembly: System.Reflection.AssemblyTitleAttribute("14Interface")]
+[assembly: System.Reflection.AssemblyProductAttribute("ProgAssincrona")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ProgAssincrona")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
