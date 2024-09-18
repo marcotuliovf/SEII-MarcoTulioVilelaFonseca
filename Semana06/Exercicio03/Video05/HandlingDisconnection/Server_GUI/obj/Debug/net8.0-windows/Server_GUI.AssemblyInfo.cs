b76c00edc24f232f4eb6c53ee33eed9f1b163a68
Marco Tulio V Fonseca <marcotuliov.fonseca@hotmail.com>
2024-09-18 17:37:53 -0300

    Arquivos atualizados com sucesso!

--- Semana06/Exercicio03/Video05/HandlingDisconnection/Server_GUI/obj/Debug/net8.0-windows/Server_GUI.AssemblyInfo.cs
+++ Semana06/Exercicio03/Video05/HandlingDisconnection/Server_GUI/obj/Debug/net8.0-windows/Server_GUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server_GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5cd97f8bbe43462fbd7ff7f97eaa4c127fd2db8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2de3fa204baffff6a19929b3c951048134ef77fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server_GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server_GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
